b39e2df3d5e9e5a4122b7eda5a85b44d2605a821
dhblum <dhblum@lbl.gov>
2017-05-26 09:04:52 -0700

    Setup documentation for occupant package.

---
+++ occupant/adaptive/__init__.py
@@ -0,0 +1,8 @@
+#
+"""
+Adaptive behavior models consider how occupants adapt to space conditions.
+These include models that predict occupant preferences for environmental
+conditions and occupant interactions with building elements, such as HVAC, 
+facade, lighting, and plug loads.
+
+"""
---
+++ occupant/occupancy/__init__.py
@@ -0,0 +1,11 @@
+#
+"""
+Occupancy models consider when occupants arrive or depart a space 
+(or building) as well as how many occupants are present at a particular time.
+
+Models
+======
+
+.. automodule:: occupant.occupancy.queueing
+
+"""
