ad66d8533851527552b920d8bcaaeaa1fe52c026
dhblum <dhblum@lbl.gov>
2017-06-09 16:48:27 -0700

    Update unittests for systems.

--- unittests/testing.py
+++ unittests/testing.py
@@ -8,7 +8,6 @@ from abc import ABCMeta
 import unittest
 from mpcpy import utility
 import pandas as pd
-from pandas.util.testing import assert_frame_equal
 import os
 
 class TestCaseMPCPy(unittest.TestCase):
@@ -26,11 +25,13 @@ class TestCaseMPCPy(unittest.TestCase):
     def check_df_timeseries(self, df_test, ref_file_name):
         # Define reference file
         ref_file_path = self.get_ref_path() + '/' + ref_file_name;
+        # Check if reference file exists
         try:
-            df_ref = pd.read_csv(ref_file_path, index_col='Time');
+            df_ref = pd.read_csv(ref_file_path, index_col=0);
             df_ref.index = pd.to_datetime(df_ref.index);
             df_ref = df_ref.tz_localize('UTC')
-            assert_frame_equal(df_test, df_ref);
+            pd.util.testing.assert_frame_equal(df_test, df_ref);
+        # If reference file does not exist, create one
         except IOError:
             ref_file_dir = self.get_ref_path();
             if not os.path.exists(ref_file_dir):
@@ -40,9 +41,11 @@ class TestCaseMPCPy(unittest.TestCase):
     def check_df_parameter(self, df_test, ref_file_name):
         # Define reference file
         ref_file_path = self.get_ref_path() + '/' + ref_file_name;
+        # Check if reference file exists
         try:
             df_ref = pd.read_csv(ref_file_path, index_col='Name');
-            assert_frame_equal(df_test, df_ref, check_dtype=False);
+            pd.util.testing.assert_frame_equal(df_test, df_ref, check_dtype=False);
+        # If reference file does not exist, create one
         except IOError:
             ref_file_dir = self.get_ref_path();
             if not os.path.exists(ref_file_dir):
